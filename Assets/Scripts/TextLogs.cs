56133f2852bdfd21415f054bb9aa9675e19d60fb
jiwon <knowledgeis1ne@gmail.com>
2023-06-12 18:41:14 +0900

    아이템 습득 로그 UI 및 위치 변경, 스크립트 키 변경(엔터)

--- Assets/Scripts/TextLogs.cs
+++ Assets/Scripts/TextLogs.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextLogs : MonoBehaviour
 {
     public static TextLogs instance;
+    [SerializeField] private GameObject logPrefab;
+    [SerializeField] private TextMeshProUGUI logPrefabText;
+    [SerializeField] private Transform tf_logPrefab;
 
-    public TextMeshProUGUI textPrefab;
-    public Transform tf_textPrefab;
-
-    private List<TextMeshProUGUI> logList = new List<TextMeshProUGUI>();
-
+    //private List<GameObject> logList = new List<GameObject>();
+          
     private string log;
 
     private void Start()
@@ -23,14 +24,13 @@ public class TextLogs : MonoBehaviour
     public void GetItemLog(int _itemID) // 아이템 획득 로그
     {
         for (int i = 0; i < ItemDatabase.itemList.Count; i++)
-        {
             if (ItemDatabase.itemList[i].itemID == _itemID)
             {
                 log = (ItemDatabase.itemList[i].itemName + "을/를 획득했습니다");
+                break;
             }
-        }
 
-        CreateLog();
+        CreateLog(log);
     }
 
     public void GetTextLog(string _log)
@@ -43,30 +43,32 @@ public class TextLogs : MonoBehaviour
 
     }
 
-    private void CreateLog() // 로그 생성
+    private void CreateLog(string _log) // 로그 생성
     {
-        TextMeshProUGUI textLogs = Instantiate(textPrefab, tf_textPrefab).GetComponent<TextMeshProUGUI>();
-        textLogs.text = log;
-        logList.Add(textLogs);
-        Debug.Log(logList);
-        StartCoroutine("FadeOutStart", textLogs);
+        GameObject log = Instantiate(logPrefab, tf_logPrefab);
+        TextMeshProUGUI logText = log.transform.Find("TextPrefab").GetComponent<TextMeshProUGUI>();
+        logText.text = _log;
+        log.SetActive(true);
+        logText.color = Color.white;
+        StartCoroutine(FadeOutStart(log, logText));
     }
 
-    private IEnumerator FadeOutStart(TextMeshProUGUI _textLogs) // 로그 페이드아웃 후 파괴
+    private IEnumerator FadeOutStart(GameObject _textLogs, TextMeshProUGUI _text) // 로그 페이드아웃 후 파괴
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSecondsRealtime(3f); // 3초 뒤 파괴되도록 함
 
-        Color c = _textLogs.color;
+        Color32 c1 = _textLogs.GetComponent<Image>().color; // 페이드아웃을 위해 로그의 컬러값을 받아 옴
+        Color32 c2 = _text.color;
 
-        for (float f = 1f; f >= 0f; f -= 0.01f)
+        for (float f = 1f; f >= 0f; f -= 0.01f) // 페이드아웃
         {
-            c.a = f;
-            _textLogs.color = c;
+            c1.a = (byte)(f * 40); 
+            c2.a = (byte)(f * 255);
+            _textLogs.GetComponent<Image>().color = c1;
+            _text.color = c2;
             yield return null;
         }
 
-        Destroy(_textLogs);
+        Destroy(_textLogs); // 페이드아웃이 끝나면 로그가 파괴됨
     }
-
-
 }
