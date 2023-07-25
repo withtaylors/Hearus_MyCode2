493c81e93e93d195a9c8159e70f90daf38daf38b
jiwon <knowledgeis1ne@gmail.com>
2023-07-25 22:15:07 +0900

    인벤토리 세이브, 로드

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using static Item;
+using System;
 
 public class PlayerData
 {
@@ -14,6 +16,42 @@ public class PlayerData
     public float z = -73f;
 }
 
+[Serializable]
+public class InventoryData
+{
+    public int itemID;
+    public string itemName;
+    public string itemDescription;
+    public int itemCount;
+    public Sprite itemIcon;
+    public ItemType itemType;
+    public ItemEffect itemEffect;
+    public int effectValue;
+    public bool isMeet;
+    public bool isPicking;
+
+    public InventoryData(int _itemID, string _itemName, string _itemDes, ItemType _itemType, ItemEffect _itemEffect, int _effectValue, int _itemCount = 1, bool _isMeet = false, bool _isPicking = false)   // 생성자
+    {
+        // itemID는 아이콘 파일의 이름과 같게 함
+        itemID = _itemID;
+        itemName = _itemName;
+        itemDescription = _itemDes;
+        itemType = _itemType;
+        itemCount = _itemCount;
+        itemIcon = Resources.Load("ItemIcon/" + _itemID.ToString(), typeof(Sprite)) as Sprite;
+        itemEffect = _itemEffect;
+        effectValue = _effectValue;
+        isMeet = _isMeet;
+        isPicking = _isPicking;
+    }
+}
+
+[Serializable]
+public class InventoryDataWrapper
+{
+    public List<InventoryData> items;
+}
+
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance; // 싱글톤패턴
@@ -21,9 +59,10 @@ public class DataManager : MonoBehaviour
     public PlayerData nowPlayer = new PlayerData(); // 플레이어 데이터 생성
     public PlayerData nowPlayerDefault = new PlayerData(); // 기본 플레이어 데이터 생성
 
+    public InventoryDataWrapper dataWrapper = new InventoryDataWrapper();
+
     public string path; // 경로
     public int nowSlot; // 현재 슬롯번호
-
 
     private void Awake()
     {
@@ -50,19 +89,50 @@ public class DataManager : MonoBehaviour
 
     public void SaveData()
     {
-        string data = JsonUtility.ToJson(nowPlayer);
-        File.WriteAllText(path + nowSlot.ToString(), data);
+        string playerData = JsonUtility.ToJson(nowPlayer);
+        File.WriteAllText(path + nowSlot.ToString() + "_player.json", playerData);
+
+        dataWrapper.items = new List<InventoryData>();
+        for (int i = 0; i < InventoryDataManager.Instance.inventoryItemList.Count; i++)
+        {
+            Item _item = InventoryDataManager.Instance.inventoryItemList[i];
+            InventoryData _inventoryData = new InventoryData(_item.itemID, _item.itemName, _item.itemDescription,
+                _item.itemType, _item.itemEffect, _item.effectValue, _item.itemCount, _item.isMeet, _item.isPicking);
+            dataWrapper.items.Add(_inventoryData);
+            Debug.Log(_inventoryData.itemID);
+        }
+        string inventoryData = JsonUtility.ToJson(dataWrapper);
+        File.WriteAllText(path + nowSlot.ToString() + "_inventory.json", inventoryData);
     }
 
     public void LoadData()
     {
-        string data = File.ReadAllText(path + nowSlot.ToString());
-        nowPlayer = JsonUtility.FromJson<PlayerData>(data);
+        string playerData = File.ReadAllText(path + nowSlot.ToString() + "_player.json");
+        nowPlayer = JsonUtility.FromJson<PlayerData>(playerData);
+
+        string inventoryData = File.ReadAllText(path + nowSlot.ToString() + "_inventory.json");
+        InventoryDataWrapper dataWrapper = JsonUtility.FromJson<InventoryDataWrapper>(inventoryData);
+
+        if (dataWrapper != null && dataWrapper.items != null)
+        {
+            List<InventoryData> itemsToLoad = dataWrapper.items;
+
+            if (itemsToLoad.Count > 0)
+                for (int i = 0; i < itemsToLoad.Count; i++)
+                {
+                    Item _item = new Item(itemsToLoad[i].itemID, itemsToLoad[i].itemName, itemsToLoad[i].itemDescription,
+                    itemsToLoad[i].itemType, itemsToLoad[i].itemEffect, itemsToLoad[i].effectValue, itemsToLoad[i].itemCount,
+                    itemsToLoad[i].isMeet, itemsToLoad[i].isPicking);
+
+                    InventoryDataManager.Instance.inventoryItemList.Add(_item);
+                }
+        }
     }
 
     public void DataClear()
     {
         nowSlot = -1;
         nowPlayer = new PlayerData();
+        InventoryDataManager.Instance.inventoryItemList.Clear();
     }
 }
--- Assets/Scripts/Inventory_MainGame/ItemPickup.cs
+++ Assets/Scripts/Inventory_MainGame/ItemPickup.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using Yarn.Unity;
+using UnityEngine.UIElements;
 
 public class ItemPickup : MonoBehaviour
 {
@@ -11,13 +12,37 @@ public class ItemPickup : MonoBehaviour
     public int _count;
 
     private ScriptManager scriptManager;
+    private UnityEngine.Camera selectedCamera;
+    private Vector3 objectPosition;
+    private Quaternion objectRotation;
+
+    public ParticleSystem particlePrefab;
 
     private void Start()
     {
         scriptManager = FindObjectOfType<ScriptManager>();
+        selectedCamera = UnityEngine.Camera.main;
+
+        objectPosition = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        objectRotation = new Quaternion(-90, 0, 0, 90);
+
+        //particlePrefab
+
+        //particlePrefab.Pause();
+        //particlePrefab.GetComponent<ObjectParticleSystem>().StopParticle();
     }
 
-    public void Pickup(GameObject item)
+    private void FixedUpdate()
+    {
+        if (CheckObjectInCamera(gameObject))
+        {
+            Debug.Log("CheckObjectInCamera()");
+            //particlePrefab.Play();
+            //particlePrefab.GetComponent<ObjectParticleSystem>().PlayParticle();
+        }
+    }
+
+    public void Pickup(GameObject item) // 아이템 줍기
     {
         int pickingID;
         int pickingCount;
@@ -30,5 +55,16 @@ public class ItemPickup : MonoBehaviour
 
         TextLogs.instance.GetItemLog(pickingID);
         Inventory.instance.GetAnItem(pickingID, pickingCount);
+    }
+    
+    private bool CheckObjectInCamera(GameObject item) // 오브젝트가 카메라 안에 있는지 확인
+    {
+        Vector3 itemPos = selectedCamera.WorldToViewportPoint(item.transform.position);
+
+        if (itemPos.x >= 0 && itemPos.x <= 1 &&
+            itemPos.y >= 0 && itemPos.y <= 1 && itemPos.z > 0)
+            return true;
+        else
+            return false;
     }
 }
