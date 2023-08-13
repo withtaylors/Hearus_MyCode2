2e6983fb818b71aaba72a455bedecc6efb5160bb
withtaylors <withtaylors@naver.com>
2023-08-13 22:21:53 +0900

    배경음오류전체적으로수정 + 안쓰는코드버림

--- Assets/Scripts/Sound/bgmSound.cs
+++ Assets/Scripts/Sound/bgmSound.cs
@@ -8,12 +8,18 @@ public class bgmSound : MonoBehaviour
     public AudioSource audioSource;
     public Slider volumeSlider;
     public float fadeInTime = 1.0f; // 볼륨 증가에 걸리는 시간, 초단위
-    public float fadeOutTime = 1.0f; // 볼륨 감소에 걸리는 시간, 초단위
+    public float fadeOutTime = 1.5f; // 볼륨 감소에 걸리는 시간, 초단위
 
     void Awake()
     {
         // AudioSource의 기본적인 소리크기 설정
-        audioSource.volume = 0.3f;
+        audioSource.volume = 0.2f;
+
+        // 슬라이더 초기 값을 AudioSource의 볼륨과 일치시킵니다.
+        if (volumeSlider != null) // 슬라이더가 할당되어 있어야 함을 확인합니다.
+        {
+            volumeSlider.value = audioSource.volume;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -27,7 +33,7 @@ public class bgmSound : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && audioSource.isPlaying)
         {
             StartCoroutine(FadeOut(audioSource, fadeOutTime));
         }
@@ -35,7 +41,7 @@ public class bgmSound : MonoBehaviour
 
     public void OnVolumeChanged(float value)
     {
-        // 슬라이더가 할당되어 있으면 볼륨을 설정합니다.
+        // 슬라이더가 할당되어 있으면 볼륨을 설정
         if (volumeSlider != null)
         {
             audioSource.volume = value;
@@ -47,16 +53,16 @@ public class bgmSound : MonoBehaviour
         float startVolume = 0.0f;
         audioSource.volume = startVolume;
 
-        while (audioSource.volume < 0.3f)
+        while (audioSource.volume < 0.2f)
         {
             audioSource.volume += 0.3f * Time.deltaTime / duration;
             yield return null;
         }
 
-        audioSource.volume = 0.3f;
+        audioSource.volume = 0.2f;
     }
 
-   IEnumerator FadeOut(AudioSource audioSource, float duration)
+    IEnumerator FadeOut(AudioSource audioSource, float duration)
     {
         float startVolume = audioSource.volume;
 
