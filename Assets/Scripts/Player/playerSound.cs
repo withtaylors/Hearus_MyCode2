ee710480421f20a9d15812556dddf37ed1698ec3
withtaylors <withtaylors@naver.com>
2023-08-17 21:53:35 +0900

    Player Control + Sound 수정완료

--- Assets/Scripts/Player/playerSound.cs
+++ Assets/Scripts/Player/playerSound.cs
@@ -15,6 +15,7 @@ public class playerSound : MonoBehaviour
     public AudioSource climbSound;
 
     private playerController player;
+    private Coroutine fadeOutCoroutine; // 소리가 점점 작아지게끔 하는 코루틴을 저장할 변수
 
     void Start()
     {
@@ -105,19 +106,35 @@ public class playerSound : MonoBehaviour
         }
         wasPicking = player.isPicking;
 
-         // 밧줄 타기 소리를 재생하거나 정지합니다.
-        if (player.isClimbing)
+        if (player.isClimbing) // isClimbing을 기준으로 소리를 재생하거나 정지
         {
-            if (!climbSound.isPlaying)
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0)
             {
-                climbSound.Play();
+                if (!climbSound.isPlaying)
+                {
+                    climbSound.volume = 1; // 사운드를 다시 키면 볼륨을 원래 크기로 재생
+                    climbSound.Play();
+                }
+
+                if (fadeOutCoroutine != null) // 이미 fadeOut 코루틴이 실행 중이면 중지
+                {
+                    StopCoroutine(fadeOutCoroutine);
+                    fadeOutCoroutine = null;
+                }
+            }
+            else if (climbSound.isPlaying) // 입력이 없을 경우 점차적으로 소리 크기를 줄임
+            {
+                if (fadeOutCoroutine == null) // fadeOut 코루틴이 실행 중이지 않으면 시작
+                {
+                    fadeOutCoroutine = StartCoroutine(FadeOutSound(0.3f));
+                }
             }
         }
-        else
+        else if (!player.isClimbing && climbSound.isPlaying)
         {
-            if (climbSound.isPlaying)
+            if (fadeOutCoroutine == null) // fadeOut 코루틴이 실행 중이지 않으면 시작
             {
-                climbSound.Stop();
+                fadeOutCoroutine = StartCoroutine(FadeOutSound(0.3f));
             }
         }
     }
@@ -126,5 +143,24 @@ public class playerSound : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         pickSound.Play();
+    }
+
+    // 0.5 초 동안 점차적으로 소리를 줄여 주는 코루틴
+    IEnumerator FadeOutSound(float duration)
+    {
+        float initialVolume = climbSound.volume;
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            // 볼륨을 점차적으로 줄임
+            climbSound.volume = Mathf.Lerp(initialVolume, 0, elapsedTime / duration);
+            yield return null;
+        }
+
+        climbSound.Stop(); // 소리 완전히 중단
+        climbSound.volume = initialVolume; // 볼륨을 원래 값으로 설정
+        fadeOutCoroutine = null; // 코루틴 상태를 초기화
     }
 }
