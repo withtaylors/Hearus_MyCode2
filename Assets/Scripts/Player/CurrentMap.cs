851444e87321093e02fafb8a073f8b1b79fead3b
jiwon <knowledgeis1ne@gmail.com>
2023-08-29 17:31:32 +0900

    Border 밟으면 스테이지 이름 업데이트되게 수정

--- Assets/Scripts/Player/CurrentMap.cs
+++ Assets/Scripts/Player/CurrentMap.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SocialPlatforms;
 
 public class CurrentMap : MonoBehaviour
 {
+    public TextMeshProUGUI stageText_1;
+    public TextMeshProUGUI stageText_2;
+    public TextMeshProUGUI stageText_3;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name.Equals("중심들판_Border_1")) // 중심들판 <-> 빽빽한숲
-            Debug.Log("중심들판_Border_1");
+            UpdateCurrentStage("중심 들판");
 
         if (collision.gameObject.name.Equals("빽빽한숲_Border_1")) // 중심들판 <-> 빽빽한숲
-            Debug.Log("빽빽한숲_Border_1");
+            UpdateCurrentStage("빽빽한 숲");
 
         if (collision.gameObject.name.Equals("빽빽한숲_Border_2")) // 빽빽한숲 <-> 시냇물숲
-            Debug.Log("빽빽한숲_Border_1");
+            UpdateCurrentStage("빽빽한 숲");
 
         if (collision.gameObject.name.Equals("시냇물숲_Border_1")) // 빽빽한숲 <-> 시냇물숲
-            Debug.Log("빽빽한숲_Border_1");
+            UpdateCurrentStage("시냇물이 흐르는 숲");
+    }
+
+    private void UpdateCurrentStage(string _stage)
+    {
+        stageText_1.text = _stage;
+        stageText_2.text = _stage;
+        stageText_3.text = _stage;
     }
 }
