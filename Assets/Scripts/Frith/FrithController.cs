cbe9315930953949192c8eec305c3c03fbc9ae50
withtaylors <withtaylors@naver.com>
2023-09-04 21:20:13 +0900

    프리스 이동 자연스럽게 설정중 _ navmesh는X, 코드로 다시 진행중_중간저장

--- Assets/Scripts/Frith/FrithController.cs
+++ Assets/Scripts/Frith/FrithController.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class FrithController : MonoBehaviour
 {
     public Transform player; // 플레이어 오브젝트
-    public float followSpeed = 5f; // 펫의 따라가는 속도
-    public float verticalSpeed = 1f; // 위아래로 움직이는 속도
-    public float verticalRange = 0.5f; // 위아래 움직임의 범위
+    public float followSpeed = 3f; // 펫의 따라가는 속도
+    public float verticalSpeed = 2f; // 위아래로 움직이는 속도
+    public float verticalRange = 1f; // 위아래 움직임의 범위
 
     private Vector3 initialOffset; // 초기 위치 오프셋
     private Quaternion initialRotationOffset; // 초기 회전 오프셋
 
     private void Start()
     {
-        initialOffset = transform.position - player.position;
+        initialOffset = player.right * -1f + player.up + player.forward * 2f; // 플레이어의 왼쪽 상단에 위치하도록 수정
+        transform.position = player.position + initialOffset;
+
         initialRotationOffset = Quaternion.Euler(0f, 90f, 0f); // 초기 회전 각도를 설정합니다.
     }
 
--- Assets/Scripts/Frith/FrithControllerTest.cs
+++ Assets/Scripts/Frith/FrithControllerTest.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class FrithControllerTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float distance;
+    public float jumpPower;
+
+    public LayerMask groundLayer;
+
+    Transform player;
+
+    Rigidbody rig;
+
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player").transform;
+        Physics.IgnoreLayerCollision(9, 10);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 펫의 y 위치를 플레이어의 y 위치보다 항상 높게 유지
+        Vector3 newPosition = transform.position;
+        newPosition.y = Mathf.Max(newPosition.y, player.position.y);
+
+        if (Mathf.Abs(transform.position.x - player.position.x) > distance)
+        {
+            Vector3 moveDirection = (player.position - transform.position).normalized;
+            newPosition += moveDirection * speed * Time.deltaTime;
+            DirectionPet();
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.left, out hit, 0.5f, groundLayer))
+            {
+                rig.velocity = Vector3.up * jumpPower;
+            }
+        }
+
+        // 펫의 위치를 업데이트
+        transform.position = newPosition;
+    }
+
+    void DirectionPet()
+    {
+        if (transform.position.x - player.position.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }
