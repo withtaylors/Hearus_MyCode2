7c4edb05381fa8c262b3127d180670e3da93da0f
jiwon <knowledgeis1ne@gmail.com>
2023-09-15 19:19:59 +0900

    현재 맵 이름 UI 업데이트 함수 추가

--- Assets/Scripts/Player/CurrentMap.cs
+++ Assets/Scripts/Player/CurrentMap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SocialPlatforms;
+using UnityEngine.SceneManagement;
 
 public class CurrentMap : MonoBehaviour
 {
@@ -10,7 +11,11 @@ public class CurrentMap : MonoBehaviour
     public TextMeshProUGUI stageText_2;
     public TextMeshProUGUI stageText_3;
 
-    void OnCollisionEnter(Collision collision)
+    public TextMeshProUGUI mapText_1;
+    public TextMeshProUGUI mapText_2;
+    public TextMeshProUGUI mapText_3;
+
+    void OnCollisionEnter(Collision collision) // 현재 스테이지를 파악하기 위함
     {
         if (collision.gameObject.name.Equals("중심들판_Border_1")) // 중심들판 <-> 빽빽한숲
             UpdateCurrentStage("중심 들판");
@@ -25,10 +30,17 @@ public class CurrentMap : MonoBehaviour
             UpdateCurrentStage("시냇물이 흐르는 숲");
     }
 
-    private void UpdateCurrentStage(string _stage)
+    private void UpdateCurrentStage(string _stage) // 현재 스테이지 변경
     {
         stageText_1.text = _stage;
         stageText_2.text = _stage;
         stageText_3.text = _stage;
+    }
+
+    public void UpdateCurrentMap(string _map) // 현재 맵 변경 -> 추후 씬이 변경될 떄 호출
+    {
+        mapText_1.text = _map;
+        mapText_2.text = _map;
+        mapText_3.text = _map;
     }
 }
