bd55b436cd64c31faf6f751a85ef280cf5f2b558
jiwon <knowledgeis1ne@gmail.com>
2023-09-16 17:41:27 +0900

    인벤토리 UI 재수정 / 크래프팅 가이드 버튼

--- Assets/Scripts/Inventory_MainGame/CraftingGuide.cs
+++ Assets/Scripts/Inventory_MainGame/CraftingGuide.cs
@@ -8,6 +8,13 @@ public class CraftingGuide : MonoBehaviour
 {
     public Transform tf_materialSlots;
 
+    public GameObject GuidePanel;
+    public GameObject DescriptionPanel;
+    public GameObject CraftingPanel;
+    public GameObject Slots;
+    public GameObject ExitButton;
+    public GameObject BackButton;
+
     private void Start()
     {
         for (int i = 0; i < tf_materialSlots.childCount; i++)
@@ -29,5 +36,26 @@ public class CraftingGuide : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void OnGuideButtonClick()
+    {
+        DescriptionPanel.SetActive(false);
+        CraftingPanel.SetActive(false);
+        Slots.SetActive(false);
+        ExitButton.SetActive(false);
+
+        GuidePanel.SetActive(true);
+        BackButton.SetActive(true);
+    }
+
+    public void OnBackButtonClick()
+    {
+        DescriptionPanel.SetActive(true);
+        CraftingPanel.SetActive(true);
+        Slots.SetActive(true);
+        ExitButton.SetActive(true);
+
+        GuidePanel.SetActive(false);
     }
 }
