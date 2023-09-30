fc94a090e71977996eefd4b309db6e6d25edfcf2
withtaylors <withtaylors@naver.com>
2023-10-01 01:54:10 +0900

    프리스 떠다니게 추가

--- Assets/Scripts/Frith/FrithControllerTest5.cs
+++ Assets/Scripts/Frith/FrithControllerTest5.cs
@@ -1,50 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class FrithControllerTest5 : MonoBehaviour
 {
     public float speed;
-    public float distance;
-
     public float verticalSpeed = 2f; // 위아래로 움직이는 속도
     public float verticalRange = 1f; // 위아래 움직임의 범위
 
     public LayerMask groundLayer;
     public Transform player;
 
-    private Vector3 initialPosition;
-
     public float distanceBehindPlayer = 2.0f; // 플레이어 뒤의 거리를 조절
-    private const float HeightAbovePlayer = 3.0f; // 플레이어보다 얼마나 위에 있을지 결정
 
-   void Start()
-   {
+
+    void Start()
+    {
         player = GameObject.Find("Player").transform;
-        initialPosition = transform.position + new Vector3(0, HeightAbovePlayer, 0); // 초기 위치 저장
-   }
+    }
+    void Update()
+    {
+        float yOffset = FloatUpDown();
 
-   void Update()
-   {
-       FloatUpDown(); 
-
-       Vector3 targetPosition = player.position - player.forward * distanceBehindPlayer;
-       targetPosition.y += HeightAbovePlayer;
-       transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+        // 플레이어를 따라가도록 펫의 위치를 설정
+        Vector3 targetPosition = player.position - player.forward * distanceBehindPlayer;
+        targetPosition.y += 3 + yOffset; // 여기에 yOffset 추가
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 50);
         
-       Vector3 lookAtPosition = player.position;
-       lookAtPosition.y += 1.5f; 
-       transform.LookAt(lookAtPosition);
+        Vector3 lookAtPosition = player.position;
+        lookAtPosition.y = transform.position.y; // 펫과 플레이어의 높이가 같아야 함
+        transform.LookAt(lookAtPosition);
 
-      if (Vector3.Distance(transform.position, player.position) < distance)
-      {
-           transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
-           initialPosition.y=transform.position.y;//플레이어가 앞지르면 다시 초기위치 재설정.
-      }
-   }
+        // 플레이어가 펫을 앞지르면 펫을 뒤로 이동
+        if (Vector3.Distance(transform.position, player.position) < distanceBehindPlayer)
+        {
+            transform.position = player.position - player.forward * distanceBehindPlayer;
+        }
+    }
 
-   private void FloatUpDown()
-   {
-        float yOffset = Mathf.Sin(Time.time * verticalSpeed) * verticalRange;
-        transform.position=new Vector3(transform.position.x,initialPosition.y+yOffset ,transform.position.z);
-   }
+    private float FloatUpDown()
+    {
+        // 반환값으로 sin 값 반환 
+        return Mathf.Sin(Time.time * verticalSpeed) * verticalRange;
+    }
 }
