6b9ec7b718e8f5979220ebbbfb0cf25a6470ae77
withtaylors <withtaylors@naver.com>
2023-10-01 03:10:53 +0900

    프리스 움직임추가중

--- Assets/Scripts/Frith/FrithControllerTest5.cs
+++ Assets/Scripts/Frith/FrithControllerTest5.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class FrithControllerTest5 : MonoBehaviour
 {
-    public float speed;
+public float speed;
+    public float distance;
+
     public float verticalSpeed = 2f; // 위아래로 움직이는 속도
     public float verticalRange = 1f; // 위아래 움직임의 범위
 
@@ -18,13 +20,14 @@ public class FrithControllerTest5 : MonoBehaviour
     {
         player = GameObject.Find("Player").transform;
     }
+
     void Update()
     {
-        float yOffset = FloatUpDown();
+        FloatUpDown();
 
         // 플레이어를 따라가도록 펫의 위치를 설정
         Vector3 targetPosition = player.position - player.forward * distanceBehindPlayer;
-        targetPosition.y += 3 + yOffset; // 여기에 yOffset 추가
+        targetPosition.y += 3;
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 50);
         
         Vector3 lookAtPosition = player.position;
@@ -38,9 +41,10 @@ public class FrithControllerTest5 : MonoBehaviour
         }
     }
 
-    private float FloatUpDown()
+    private void FloatUpDown()
     {
-        // 반환값으로 sin 값 반환 
-        return Mathf.Sin(Time.time * verticalSpeed) * verticalRange;
+    // 펫을 위아래로 움직이게 하는 코드 작성
+        float yOffset = Mathf.Sin(Time.time * verticalSpeed) * verticalRange;
+        transform.Translate(Vector3.up * yOffset * Time.deltaTime); // Translate를 사용하여 움직임 적용
     }
 }
