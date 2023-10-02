ef942887d41db45362e7964c9d58596c1dd1fd54
jiwon <knowledgeis1ne@gmail.com>
2023-10-02 16:42:29 +0900

    csv 파일 수정

--- Assets/Scripts/Journey_MainGame/JourneyDataManager.cs
+++ Assets/Scripts/Journey_MainGame/JourneyDataManager.cs
@@ -32,7 +32,7 @@ public class JourneyDataManager : MonoBehaviour
 
         for (int i = 0; i <= _EndLine - _StartLine; i++)
         {
-            journeyList.Add(journeyDic[_StartLine + i]);
+            journeyList.Add(journeyDic[_StartLine + i + 1]);
         }
 
         return journeyList.ToArray();
