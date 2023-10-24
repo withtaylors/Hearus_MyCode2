0b772aecbb52e34202cc61806a44f6bd74a9ac27
jiwon <knowledgeis1ne@gmail.com>
2023-10-24 21:22:39 +0900

    프리스 선물 (구현 전)2

--- Assets/Scripts/Inventory_MainGame/PresentToFrith.cs
+++ Assets/Scripts/Inventory_MainGame/PresentToFrith.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PresentToFrith : MonoBehaviour
 {
+    public Transform recievePanel;
+    public Transform refusePanel;
+
     public void OnPresentButtonClick() // 인벤토리에서 선물 버튼을 눌렀을 때
     {
         // 선물이 가능한 아이템인지 조건 검사
 
         // 선물이 불가능하다면
         // 프리스는 선물을 받지 않았다는 메시지 출력
+        refusePanel.gameObject.SetActive(true);
 
         // 선물이 가능하다면
         // 인벤토리에서 해당 아이템을 1개 삭제
         // 프리스가 선물을 받았다는 메시지 출력
+        recievePanel.gameObject.SetActive(true);
     }
 
     public void OnPresentChoice() // 프리스에게 준다는 선택지를 골랐을 때
@@ -22,8 +27,15 @@ public class PresentToFrith : MonoBehaviour
 
         // 선물이 불가능하다면
         // 프리스는 선물을 받지 않았다는 메시지 출력
+        refusePanel.gameObject.SetActive(true);
 
         // 선물이 가능하다면
         // 프리스가 선물을 받았다는 메시지 출력
+        recievePanel.gameObject.SetActive(true);
     }
+
+    //private IEnumerator FadeOutPanel()
+    //{
+
+    //}
 }
